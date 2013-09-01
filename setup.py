63985a7bdf6899760a27495ed9789826ea0e50df
Michael Bauer <mihi@lo-res.org>
2013-09-01 19:59:16 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(name='pylittlesis', version='0.01',
+            py_modules=['littlesis'],
+            url="https://github.com/mihi-tr/pylittlesis",
+            author="Michael Bauer",
+            author_email="mihi@lo-res.org",
+            description="""A Python wrapper around the Little Sis API""",
+            long_description="""
+pylittlesis - A Python library to wrap the Little Sis API
+=========================================================
+
+description...
+
+
+usage
+-----
+
+
+"""
+                  )
